7e897ab85ce50e090e4c670f8fa70547e1564c6c
Darion Valdez <darionvldz@gmail.com>
2024-05-29 20:25:09 +0200

    add make computer move function

--- script.js
+++ script.js
@@ -1,30 +1,129 @@
 const gameRenderer = (function () {
+  const computerMove = document.querySelector("#computer-move");
+  const playerMove = document.querySelector("#player-move");
+
   const playerScore = document.querySelector("#player-score");
   const computerScore = document.querySelector("#computer-score");
 
   const renderPlayerScore = (value) => (playerScore.textContent = value);
   const renderComputerScore = (value) => (computerScore.textContent = value);
 
+  const renderComputerMove = (move) => {
+    switch (move) {
+      case 0:
+        computerMove.src = "/images/choices/computer_rock.gif";
+        break;
+      case 1:
+        computerMove.src = "/images/choices/computer_paper.gif";
+        break;
+      case 2:
+        computerMove.src = "/images/choices/computer_scissors.gif";
+        break;
+      default:
+        console.log("Unable to render computer move");
+        break;
+    }
+  };
+
+  const renderPlayerMove = (move) => {
+    switch (move) {
+      case 0:
+        playerMove.src = "/images/choices/player_rock.gif";
+        break;
+      case 1:
+        playerMove.src = "/images/choices/player_paper.gif";
+        break;
+      case 2:
+        playerMove.src = "/images/choices/player_scissors.gif";
+        break;
+      default:
+        console.log("Unable to render player move");
+        break;
+    }
+  };
+
   return {
     renderComputerScore,
     renderPlayerScore,
+    renderComputerMove,
+    renderPlayerMove,
   };
 })();
 
 const gameController = (function () {
+  const buttons = document.querySelectorAll("button");
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      switch (button.className) {
+        case "choice-button":
+          makeComputerMove();
+          break;
+        default:
+          break;
+      }
+    });
+  });
+
   let playerScore = 0;
   let computerScore = 0;
+
+  let computerSelection = -1;
+  let playerSelection = -1;
 
   const getPlayerScore = () => playerScore;
   const getComputerScore = () => computerScore;
 
-  const increasePlayerScore = () => playerScore++;
-  const increaseComputerScore = () => computerScore++;
+  const getPlayerSelection = () => playerSelection;
+  const getComputerSelection = () => computerSelection;
+
+  const setPlayerSelection = (value) => (playerSelection = value);
+  const setComputerSelection = (value) => (computerSelection = value);
+
+  const increasePlayerScore = () => {
+    playerScore++;
+    gameRenderer.renderPlayerScore(playerScore);
+  };
+  const increaseComputerScore = () => {
+    computerScore++;
+    gameRenderer.renderComputerScore(computerScore);
+  };
+
+  const resetScores = () => {
+    playerScore = 0;
+    computerScore = 0;
+    gameRenderer.renderComputerScore(computerScore);
+    gameRenderer.renderPlayerScore(playerScore);
+  };
+
+  //   0 = Sten, 1 = Sax, 2 = Påse
+  const generateRandomMove = () => {
+    return Math.floor(Math.random() * 3);
+  };
+
+  function makeComputerMove() {
+    setTimeout(() => {
+      console.log("test");
+    }, 1500);
+    const move = generateRandomMove();
+    gameRenderer.renderComputerMove(move);
+    computerSelection = move;
+  }
 
   return {
     getComputerScore,
     getPlayerScore,
     increaseComputerScore,
     increasePlayerScore,
+    resetScores,
+    makeComputerMove,
+    getPlayerSelection,
+    getComputerSelection,
+    setPlayerSelection,
+    setComputerSelection,
   };
+})();
+
+const gameLoop = (function () {
+  gameController.resetScores();
 })();
