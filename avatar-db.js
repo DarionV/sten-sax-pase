513511aa413c5d687dda23b38fd57ea54e3cf841
Darion Valdez <darionvldz@gmail.com>
2024-06-04 21:18:20 +0200

    separated avatar arrays to module

---
+++ avatar-db.js
@@ -0,0 +1,78 @@
+export const computerAvatars = [
+  {
+    name: "EasyBot",
+    avatar: "images/avatars/robot_easy.png",
+    backgroundColor: "#6CC45E",
+    shadowColor: "#699C65",
+  },
+  {
+    name: "MediumBot",
+    avatar: "images/avatars/robot_medium.png",
+    backgroundColor: "#F38345",
+    shadowColor: "#A9592D",
+  },
+  {
+    name: "HardBot",
+    avatar: "images/avatars/robot_hard.png",
+    backgroundColor: "#FB5555",
+    shadowColor: "#882A2A",
+  },
+];
+
+export const playerAvatars = [
+  {
+    avatar: "images/avatars/bee.png",
+    backgroundColor: "#D95B97",
+    shadowColor: "#A14F7B",
+  },
+  {
+    avatar: "images/avatars/bird.png",
+    backgroundColor: "#64D4F8",
+    shadowColor: "#548EB8",
+  },
+  {
+    avatar: "images/avatars/cat.png",
+    backgroundColor: "#C8A6D9",
+    shadowColor: "#9679A5",
+  },
+  {
+    avatar: "images/avatars/chicken.png",
+    backgroundColor: "#D6C56B",
+    shadowColor: "#8D8052",
+  },
+  {
+    avatar: "images/avatars/cow.png",
+    backgroundColor: "#A1AE86",
+    shadowColor: "#6F7A67",
+  },
+  {
+    avatar: "images/avatars/dog.png",
+    backgroundColor: "#EC9898",
+    shadowColor: "#BC7B7B",
+  },
+  {
+    avatar: "images/avatars/fish.png",
+    backgroundColor: "#2CA0E1",
+    shadowColor: "#3F598C",
+  },
+  {
+    avatar: "images/avatars/koala.png",
+    backgroundColor: "#E5AC68",
+    shadowColor: "#916B48",
+  },
+  {
+    avatar: "images/avatars/monkey.png",
+    backgroundColor: "#80A17D",
+    shadowColor: "#425A37",
+  },
+  {
+    avatar: "images/avatars/penguin.png",
+    backgroundColor: "#E4E4E4",
+    shadowColor: "#828282",
+  },
+  {
+    avatar: "images/avatars/pig.png",
+    backgroundColor: "#AE977C",
+    shadowColor: "#644F40",
+  },
+];
